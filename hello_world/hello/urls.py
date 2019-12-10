d59d638f74a1bb744f9d6c0b68a47785a80101e0
dkgulee <silentgu28@gmail.com>
2019-12-10 20:38:51 +0900

    ㅇㅇㅇㅇㅇㅇ

--- hello_world/hello/urls.py
+++ hello_world/hello/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.hello_world, name='hello_world'),
     path('1', views.hello_world, name='hello_world'),
-    path('chee', views.chee, name='hello_world')
+    path('chee', views.chee, name='hello_world'),
+    path('nowtime', views.crrunt_datetime, name="current_time")
 
 ]
--- hello_world/hello/views.py
+++ hello_world/hello/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.http import HttpResponseNotFound
+import datetime
 
 # Create your views here.
 def hello_world(request):
     return HttpResponse("이득규")
 def chee(request):
     return HttpResponse("ㅅㄷㄴㅅㄴㅅ")
-
+def crrunt_datetime(requst):
+    try:
+        now = datetime.datetime.now()
+        html = "<html><body> 지금 시각은:{}시{}분{}초입니다.</body></html>".format(now.hour, now.second, now.minute)
+        return HttpResponse(html)
+    except HttpResponseNotFound as h:
+        return HttpResponseNotFound('<h1>{}</h1>'.format(h))
